16a7f0cb9d274cb167f709f8fc24d6cfeeeb11fe
Jonathan Wooldridge <anglewyrm6@gmail.com>
2025-04-24 04:49:19 -0700

    Boolean algebra functions

--- Attitude.java
+++ Attitude.java
@@ -6,92 +6,77 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Objects;
 import static org.anglewyrm.labodoom.ColorConstants.*;
 import static org.anglewyrm.labodoom.Constants.Component.*;
 
 public class Attitude {
 
-    private List<Component> activeComponents;
-    private static final Component[] ALL_COMPONENTS = {INTERESTED, LIKE, SURE, FEAR, EXCITED};
+    private Map<Component, Boolean> componentStates;
+    private static final Component[] ALL_COMPONENTS = {INTERESTED, LIKE, SURE, FEAR, INTENSITY};
 
-    private static final Map<List<Component>, String> ATTITUDE_LABELS = new HashMap<>();
+    private static final Map<Map<Component, Boolean>, String> ATTITUDE_LABELS = new HashMap<>();
 
     static {
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, SURE, FEAR, EXCITED), "Passionate Enthusiasm");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, SURE, FEAR), "Eager Excitement");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, SURE, EXCITED), "Emboldened Optimism");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, SURE), "Lighthearted Hopefulness");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, FEAR, EXCITED), "Fascinated Admiration");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, FEAR), "Curious Attraction");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE, EXCITED), "Devoted Affection");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, LIKE), "Comfortable Contentment");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, SURE, FEAR, EXCITED), "Intense Observance");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, SURE, FEAR), "Cautious Interest");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, SURE, EXCITED), "Anxious Anticipation");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, SURE), "Mild Worry");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, FEAR, EXCITED), "Investigative Suspicion");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, FEAR), "Guarded Skepticism");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED, EXCITED), "Profound Unease");
-        ATTITUDE_LABELS.put(Arrays.asList(INTERESTED), "Quiet Concern");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, SURE, FEAR, EXCITED), "Reluctant Acceptance");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, SURE, FEAR), "Diplomatic Tolerance");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, SURE, EXCITED), "Cautious Optimism");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, SURE), "Modest Satisfaction");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, FEAR, EXCITED), "Polite Interest");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, FEAR), "Casual Acknowledgment");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE, EXCITED), "Respectful Deference");
-        ATTITUDE_LABELS.put(Arrays.asList(LIKE), "Passive Compliance");
-        ATTITUDE_LABELS.put(Arrays.asList(SURE, FEAR, EXCITED), "Petrified Horror");
-        ATTITUDE_LABELS.put(Arrays.asList(SURE, FEAR), "Quiet Dread");
-        ATTITUDE_LABELS.put(Arrays.asList(SURE, EXCITED), "Vulnerable Insecurity");
-        ATTITUDE_LABELS.put(Arrays.asList(SURE), "Awkward Discomfort");
-        ATTITUDE_LABELS.put(Arrays.asList(FEAR, EXCITED), "Pain");
-        ATTITUDE_LABELS.put(Arrays.asList(FEAR), "Fearful");
-        ATTITUDE_LABELS.put(Arrays.asList(EXCITED), "Restless");
-        ATTITUDE_LABELS.put(new ArrayList<>(), "Empty Indifference");
+        // You'll need to define these labels to account for the NOT state
+        // This is just a placeholder - you'll need to map all the new combinations
+        Map<Component, Boolean> passionateEnthusiasm = new HashMap<>();
+        passionateEnthusiasm.put(INTERESTED, true);
+        passionateEnthusiasm.put(LIKE, true);
+        passionateEnthusiasm.put(SURE, true);
+        passionateEnthusiasm.put(FEAR, true);
+        passionateEnthusiasm.put(INTENSITY, true);
+        ATTITUDE_LABELS.put(passionateEnthusiasm, "Passionate Enthusiasm");
+
+        Map<Component, Boolean> notInterested = new HashMap<>();
+        notInterested.put(INTERESTED, false);
+        notInterested.put(LIKE, true);
+        notInterested.put(SURE, true);
+        notInterested.put(FEAR, true);
+        notInterested.put(INTENSITY, true);
+        ATTITUDE_LABELS.put(notInterested, "Disinterested Enthusiasm"); // Example label
+        // ... and so on for all 2^5 = 32 combinations
     }
 
-    public Attitude(boolean interested, boolean like, boolean sure, boolean fear, boolean excited) {
-        this.activeComponents = new ArrayList<>();
-        if (interested) activeComponents.add(INTERESTED);
-        if (like) activeComponents.add(LIKE);
-        if (sure) activeComponents.add(SURE);
-        if (fear) activeComponents.add(FEAR);
-        if (excited) activeComponents.add(EXCITED);
+    public Attitude(Map<Component, Boolean> componentStates) {
+        this.componentStates = new HashMap<>(componentStates);
     }
 
-    public Attitude(List<Component> components) {
-        this.activeComponents = new ArrayList<>(components);
+    public Attitude(Boolean interested, Boolean like, Boolean sure, Boolean fear, Boolean intensity) {
+        this.componentStates = new HashMap<>();
+        this.componentStates.put(INTERESTED, interested);
+        this.componentStates.put(LIKE, like);
+        this.componentStates.put(SURE, sure);
+        this.componentStates.put(FEAR, fear);
+        this.componentStates.put(INTENSITY, intensity);
     }
 
-    public List<Component> getActiveComponents() {
-        return new ArrayList<>(activeComponents);
+    public Map<Component, Boolean> getComponentStates() {
+        return new HashMap<>(componentStates);
     }
 
     public boolean isPresent(Component component) {
-        return activeComponents.contains(component);
+        return componentStates.containsKey(component) && componentStates.get(component);
     }
 
-    public void insert(Component component) {
-        if (!activeComponents.contains(component)) {
-            activeComponents.add(component);
-        }
+    public boolean isNotPresent(Component component) {
+        return componentStates.containsKey(component) && !componentStates.get(component);
     }
 
-    public void remove(Component component) {
-        activeComponents.remove(component);
+    public void set(Component component, boolean present) {
+        componentStates.put(component, present);
     }
 
     public String getLabel() {
-        return ATTITUDE_LABELS.getOrDefault(activeComponents, "error");
+        return ATTITUDE_LABELS.getOrDefault(componentStates, "error");
     }
 
     public String print() {
         StringBuilder sb = new StringBuilder("Attitude: ").append(getLabel()).append(" (");
         for (int i = 0; i < ALL_COMPONENTS.length; i++) {
             Component component = ALL_COMPONENTS[i];
-            String color = activeComponents.contains(component) ? LIGHT_GREEN : DARK_RED;
-            sb.append(component.getLabel()).append(": ").append(color).append(activeComponents.contains(component)).append(RESET_COLOR);
+            String color = isPresent(component) ? LIGHT_GREEN : DARK_RED;
+            sb.append(component.getLabel()).append(": ").append(color).append(componentStates.get(component)).append(RESET_COLOR);
             if (i < ALL_COMPONENTS.length - 1) {
                 sb.append(", ");
             }
@@ -102,99 +87,144 @@ public class Attitude {
 
     @Override
     public String toString() {
-        return String.format("Attitude: %s (Components: %s)", getLabel(), activeComponents);
+        return String.format("Attitude: %s (Components: %s)", getLabel(), componentStates);
     }
 
-    // New compare method
-    public static Attitude compare(Attitude a, Attitude b) {
-        List<Component> resultComponents = new ArrayList<>();
-        for (Component component : ALL_COMPONENTS) {
-            // OR operation: If the component is in either attitude, include it
-            if (a.isPresent(component) || b.isPresent(component)) {
-                resultComponents.add(component);
-            }
+    public static Attitude not(Attitude attitude) {
+        Map<Component, Boolean> negatedStates = new HashMap<>();
+        for (Map.Entry<Component, Boolean> entry : attitude.componentStates.entrySet()) {
+            negatedStates.put(entry.getKey(), !entry.getValue());
         }
-        return new Attitude(resultComponents);
+        return new Attitude(negatedStates);
+    }
+
+    public static Attitude or(Attitude a, Attitude b) {
+        Map<Component, Boolean> resultStates = new HashMap<>();
+        for (Component component : ALL_COMPONENTS) {
+            boolean presentInA = a.componentStates.getOrDefault(component, false);
+            boolean presentInB = b.componentStates.getOrDefault(component, false);
+            resultStates.put(component, presentInA || presentInB);
+        }
+        return new Attitude(resultStates);
+    }
+
+    public static Attitude and(Attitude a, Attitude b) {
+        Map<Component, Boolean> resultStates = new HashMap<>();
+        for (Component component : ALL_COMPONENTS) {
+            boolean inA = a.componentStates.getOrDefault(component, false);
+            boolean inB = b.componentStates.getOrDefault(component, false);
+            resultStates.put(component, inA && inB);
+        }
+        return new Attitude(resultStates);
+    }
+
+    public static Attitude xor(Attitude a, Attitude b) {
+        Map<Component, Boolean> resultStates = new HashMap<>();
+        for (Component component : ALL_COMPONENTS) {
+            boolean inA = a.componentStates.getOrDefault(component, false);
+            boolean inB = b.componentStates.getOrDefault(component, false);
+            resultStates.put(component, inA ^ inB);
+        }
+        return new Attitude(resultStates);
+    }
+
+    public static Attitude differenceFrom(Attitude a, Attitude b) {
+        Map<Component, Boolean> resultStates = new HashMap<>();
+        for (Component component : ALL_COMPONENTS) {
+            boolean inA = a.componentStates.getOrDefault(component, false);
+            boolean inB = b.componentStates.getOrDefault(component, false);
+            resultStates.put(component, inA && !inB);
+        }
+        return new Attitude(resultStates);
+    }
+
+    public static Attitude if_(Attitude a, Attitude b) {
+        Attitude notA = not(a);
+        return or(notA, b);
+    }
+
+    public static Attitude equivalent(Attitude a, Attitude b) {
+        Attitude aImpliesB = if_(a, b);
+        Attitude bImpliesA = if_(b, a);
+        return and(aImpliesB, bImpliesA);
+    }
+
+    public static Attitude nand(Attitude a, Attitude b) {
+        Attitude andResult = and(a, b);
+        return not(andResult);
+    }
+
+    public static Attitude nor(Attitude a, Attitude b) {
+        Attitude orResult = or(a, b);
+        return not(orResult);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Attitude other = (Attitude) obj;
+        return Objects.equals(this.componentStates, other.componentStates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(componentStates);
     }
 
     public static void main(String[] args) {
-        Random rng = new Random();
-        int sample_count = 4;
+        // Example usage with the new representation
+        Attitude a = new Attitude(true, true, false, false, true);
+        System.out.println("A: " + a);
+        System.out.println("A Label: " + a.getLabel());
 
-        for (int i = 0; i < sample_count; i++) {
-            int numComponents = rng.nextInt(ALL_COMPONENTS.length + 1);
-            List<Component> availableComponents = new ArrayList<>(Arrays.asList(ALL_COMPONENTS));
-            for (int j = 0; j < numComponents; j++) {
-                int randomIndex = rng.nextInt(availableComponents.size());
-                randomComponents.add(availableComponents.remove(randomIndex));
-            }
+        Attitude notA = Attitude.not(a);
+        System.out.println("NOT A: " + notA);
+        System.out.println("NOT A Label: " + notA.getLabel()); // You'll need to define this label
 
-            Attitude attitude = new Attitude(randomComponents);
-            System.out.println("Generated components: " + attitude.getActiveComponents());
-            System.out.println(attitude.print());
-            System.out.println("  (Evaluated: " + attitude + ")");
-            System.out.println();
-        }
+        Attitude b = new Attitude(false, true, true, true, false);
+        System.out.println("B: " + b);
+        System.out.println("B Label: " + b.getLabel());
 
-        Attitude fearExcited = new Attitude(Arrays.asList(FEAR, EXCITED));
-        System.out.println("Fear then Excited: " + fearExcited);
-        System.out.println("  Label: " + fearExcited.getLabel());
+        Attitude orAB = Attitude.or(a, b);
+        System.out.println("A OR B: " + orAB);
+        System.out.println("A OR B Label: " + orAB.getLabel()); // You'll need to define this label
 
-        Attitude likeExcited = new Attitude(Arrays.asList(LIKE, EXCITED));
-        System.out.println("Like then Excited: " + likeExcited);
-        System.out.println("  Label: " + likeExcited.getLabel());
+        Attitude andAB = Attitude.and(a, b);
+        System.out.println("A AND B: " + andAB);
+        System.out.println("A AND B Label: " + andAB.getLabel());
 
-        Attitude enthusiastic = new Attitude(Arrays.asList(INTERESTED, LIKE, SURE, FEAR, EXCITED));
-        System.out.println("Enthusiastic: " + enthusiastic);
-        System.out.println("  Label: " + enthusiastic.getLabel());
+        Attitude xorAB = Attitude.xor(a, b);
+        System.out.println("A XOR B: " + xorAB);
+        System.out.println("A XOR B Label: " + xorAB.getLabel());
 
-        printAttitudeLengthHistogram();
+        Attitude diffAB = Attitude.differenceFrom(a, b);
+        System.out.println("A differenceFrom B: " + diffAB);
+        System.out.println("A differenceFrom B Label: " + diffAB.getLabel());
 
-        // Test the compare method
-        Attitude a = new Attitude(true, true, false, false, true); // Interested, Like, Excited
-        Attitude b = new Attitude(false, true, true, true, false); // Like, Sure, Fear
-        Attitude c = Attitude.compare(a, b);
-        System.out.println("\n--- Compare Test (OR Combination) ---");
-        System.out.println("A: " + a + " Label: " + a.getLabel());
-        System.out.println("B: " + b + " Label: " + b.getLabel());
-        System.out.println("C (A OR B): " + c + " Label: " + c.getLabel());
-        System.out.println("---------------------------------------");
+        Attitude ifAB = Attitude.if_(a,b);
+        System.out.println("A if B: " + ifAB);
+        System.out.println("A if B Label: " + ifAB.getLabel());
 
-        //Demonstrate insert and remove
-        Attitude d = new Attitude(false, false, false, false, false);
-        System.out.println("\n--- insert and remove Test ---");
-        System.out.println("Initial d: " + d + " Label: " + d.getLabel());
-        d.insert(INTERESTED);
-        d.insert(EXCITED);
-        System.out.println("d after inserting Interested and Excited: " + d + " Label: " + d.getLabel());
-        if (d.isPresent(INTERESTED)) {
-            System.out.println("d is Interested");
-        } else {
-            System.out.println("d is not Interested");
-        }
-        d.remove(INTERESTED);
-        System.out.println("d after removing Interested: " + d + " Label: " + d.getLabel());
-        if (d.isPresent(INTERESTED)) {
-            System.out.println("d is Interested");
-        } else {
-            System.out.println("d is not Interested");
-        }
-        System.out.println("------------------------------------------");
+        Attitude eqAB = Attitude.equivalent(a,b);
+        System.out.println("A equivalent B: " + eqAB);
+        System.out.println("A equivalent B Label: " + eqAB.getLabel());
 
-    }
+        Attitude nandAB = Attitude.nand(a,b);
+        System.out.println("A nand B: " + nandAB);
+        System.out.println("A nand B Label: " + nandAB.getLabel());
 
-    public static void printAttitudeLengthHistogram() {
-        Map<Integer, Integer> lengthCounts = new HashMap<>();
-        for (List<Component> components : ATTITUDE_LABELS.keySet()) {
-            int length = components.size();
-            lengthCounts.put(length, lengthCounts.getOrDefault(length, 0) + 1);
-        }
+        Attitude norAB = Attitude.nor(a,b);
+        System.out.println("A nor B: " + norAB);
+        System.out.println("A nor B Label: " + norAB.getLabel());
 
-        System.out.println("\n--- Attitude Length Histogram ---");
-        for (int i = 0; i <= ALL_COMPONENTS.length; i++) {
-            int count = lengthCounts.getOrDefault(i, 0);
-            System.out.printf("%d components: %d%n", i, count);
-        }
-        System.out.println("-------------------------------");
+        System.out.println("a.equals(b): " + a.equals(b));
+        System.out.println("a.equals(a): " + a.equals(a));
+
     }
 }
+
